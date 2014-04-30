e1e45776a77f8539e7c0eafaf34c6755eeec681a
Jared Sorge <jared@jsorge.net>
2014-04-29 21:39:40 -0700

    Really finished HW4.

--- CP130_HW4/CP130_HW4/JMSBackgroundTask.m
+++ CP130_HW4/CP130_HW4/JMSBackgroundTask.m
@@ -29,12 +29,15 @@
     srand48(time(0));
     while (YES) {
         if (self.isCancelled) {
+            [self.cosbyImageView stopAnimating];
             return;
         }
         
+        UIImage *newCosby = [JMSCosbyImage randomCosbyImage];
+        
         [[NSOperationQueue mainQueue] addOperationWithBlock:^{
             self.cosbyImageView.image = nil;
-            self.cosbyImageView.image = [JMSCosbyImage randomCosbyImage];
+            self.cosbyImageView.image = newCosby;
         }];
         
         double randomSleep = (drand48() * 10);
