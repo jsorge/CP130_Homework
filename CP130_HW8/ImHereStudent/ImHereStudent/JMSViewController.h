18c990a2fe5928868a8056b8320dee6f143ef56e
Jared Sorge <jared@jsorge.net>
2014-05-27 19:19:16 -0700

    Added UI walkthrough and initial project.

---
+++ CP130_HW8/ImHereStudent/ImHereStudent/JMSViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMSViewController.h
+//  ImHereStudent
+//
+//  Created by Jared Sorge on 5/27/14.
+//  Copyright (c) 2014 net.jsorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMSViewController : UIViewController
+
+@end
---
+++ CP130_HW8/ImHereStudent/ImHereStudentTests/ImHereStudentTests.m
@@ -0,0 +1,34 @@
+//
+//  ImHereStudentTests.m
+//  ImHereStudentTests
+//
+//  Created by Jared Sorge on 5/27/14.
+//  Copyright (c) 2014 net.jsorge. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ImHereStudentTests : XCTestCase
+
+@end
+
+@implementation ImHereStudentTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
